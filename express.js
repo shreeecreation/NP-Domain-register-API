c801e5fe1a57121b19e6497fae826c0efc9430ce
shreeecreation <shreeecreation720#gmail.com>
2023-03-13 12:02:09 +0545

    Feat: Testing

--- express.js
+++ express.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended : false}));
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=> console.log(`Server is running on ${PORT}`))
 
 app.get('/get',(req,res)=>{
